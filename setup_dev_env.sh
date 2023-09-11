826615281ccd3332403ccb0e27717f16d256354a
IasonManolas <iasonmanolasece@gmail.com>
2023-09-11 12:40:34 +0300

    Changed zsh theme to p10k

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -18,8 +18,8 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 echo -e "${RED}Installing required agnoster fonts..${NC}"
 sudo apt-get install fonts-powerline
 echo -e "${RED}Cloning vimrc and tmuxrc files..${NC}"
-cp dotFiles/.* ~/
-rm -rf ../MyDevEnviroment
+cp -i dotFiles/.* ~/
+#rm -rf ../MyDevEnviroment
 echo -e "${RED}Installing vim plugin manager..${NC}"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt install clang-format
